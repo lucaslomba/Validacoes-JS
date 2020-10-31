4563f6193bb6fa2edf826defb433c3e9ef72f0ba
lucaslomba <lucasazevedolomba@gmail.com>
2020-10-31 12:24:48 -0300

    Final projeto

--- services/validar.js
+++ services/validar.js
@@ -1,80 +1,60 @@
-import { validarDataNascimento } from "./validarDataNascimento.js"
+import { validarRG } from "./validarRG.js";
+import { validarCPF } from "./validarCPF.js";
+import { validarDataNascimento } from "./validarDataNascimento.js";
+import { recuperarEndereco } from "./recuperarEndereco.js";
+import { validarPreco } from "./validarPreco.js";
+import { mensagensDeErro } from "./constants/mensagensDeErro.js";
+import { tiposDeErro } from "./constants/tiposDeErros.js";
 
-const retornarMensagemDeErro = (tipo, validity) => {
-    let mensagemDeErro = "";
-    const tiposDeErro = ["valueMissing", "typeMismatch", "tooShort", "rangeUnderflow"]
+const retornarMensagemErro = (tipo, validity) => {
+  let mensagemDeErro = "";
 
-    const mensagensDeErro = {
-        email: {
-            valueMissing: "O e-mail é necessario",
-            typeMismatch: "Este não é um e-mail valido"
-        },
-        senha: {
-            valueMissing: "A senha é necessario",
-            tooShort: "A senha deve ter no minimo 4 caracteres"
-        },
-        dataNascimento: {
-            valueMissing: "A data de nascimento é necessario",
-            rangeUnderflow: "A data minima é 01/01/1901",
-            customError: "A idade minima é de 18 anos",
-        },
-        cpf: {
-            valueMissing: "O CPF é necessario",
-        },
-        rg: {
-            valueMissing: "O RG é necessario",
-        },
-        cep: {
-            valueMissing: "O CEP é necessario",
-        },
-        logradouro: {
-            valueMissing: "O logradouro é necessario",
-        },
-        cidade: {
-            valueMissing: "A cidade é necessario",
-        },
-        estado: {
-            valueMissing: "O estado é necessario",
-        },
+  tiposDeErro.forEach(erro => {
+    if (validity[erro]) {
+      mensagemDeErro = mensagensDeErro[tipo][erro];
     }
+  });
 
-    tiposDeErro.forEach(erro => {
-        if(validity[erro]){
-            mensagemDeErro =  mensagensDeErro[tipo][erro];
-        }
-    })
-
-    return mensagemDeErro;
-}
+  return mensagemDeErro;
+};
 
 export const validarInput = (input, adicionarErro = true) => {
-    const classeElementoErro = "erro-validacao"
-    const classeInputErro = "possui-erro-validacao"
-    const elementoPai = input.parentNode;
-    const elementoErroExiste = elementoPai.querySelector(`.${classeElementoErro}`);
-    const elementoErro = elementoErroExiste || document.createElement("div")
-    const elementoEhValido = input.validity.valid;
+  const classeElementoErro = "erro-validacao";
+  const elementoPai = input.parentNode;
+  const elementoErroExiste = elementoPai.querySelector(
+    `.${classeElementoErro}`
+  );
+  const elementoErro = elementoErroExiste || document.createElement("div");
+  const classeInputErro = "possui-erro-validacao";
+  const tipo = input.dataset.tipo;
+  const elementoEhValido = input.validity.valid;
+  const validadoresEspecificos = {
+    cep: input => recuperarEndereco(input),
+    rg: input => validarRG(input),
+    cpf: input => validarCPF(input),
+    dataNascimento: input => validarDataNascimento(input),
+    preco: input => validarPreco(input)
+  };
 
-    const tipo = input.dataset.tipo;
-    const validadoresEspecificos = {
-        dataNascimento: () => validarDataNascimento(input)
-    };
+  if (validadoresEspecificos[tipo]) {
+    validadoresEspecificos[tipo](input);
+  }
 
-    if(validadoresEspecificos[tipo]){
-        validadoresEspecificos[tipo](input)
+  // elemento não é valido
+  if (!elementoEhValido) {
+    elementoErro.className = classeElementoErro;
+    elementoErro.textContent = retornarMensagemErro(
+      input.dataset.tipo,
+      input.validity
+    );
+
+    if (adicionarErro) {
+      input.after(elementoErro);
+      input.classList.add(classeInputErro);
     }
-
-    if(!elementoEhValido){
-        // Nao é valido
-        elementoErro.className = classeElementoErro;
-        elementoErro.textContent = retornarMensagemDeErro(tipo, input.validity);
-        if(adicionarErro){
-            input.after(elementoErro)
-            input.classList.add(classeInputErro);
-        }
-    }else{
-        // É valido
-        elementoErro.remove();
-        input.classList.remove(classeInputErro)
-    }
-}
+  } else {
+    // elemento é valido
+    input.classList.remove(classeInputErro);
+    elementoErro.remove();
+  }
+};
--- services/validarDataNascimento.js
+++ services/validarDataNascimento.js
@@ -1,22 +1,18 @@
 export const validarDataNascimento = input => {
-    const dataNascimento = new Date(input.value)
-    const dataAtual = new Date();
+  const dataNascimento = new Date(input.value);
+  const dataAtual = new Date();
 
-    const dataFaz18 = new Date(
-        // getUTCFullYear tras o ano que o usuario colocou
-        dataNascimento.getUTCFullYear() + 18,
-        dataNascimento.getUTCMonth(),
-        dataNascimento.getUTCDate()
-    )
+  const dataFaz18 = new Date(
+    dataNascimento.getUTCFullYear() + 18,
+    dataNascimento.getUTCMonth(),
+    dataNascimento.getUTCDate()
+  );
 
-    if( dataFaz18 > dataAtual){
-        // Menor de 18 anos
-
-        //Validação personalizada
-        input.setCustomValidity("A idade mínima é de 18 anos");
-        return;
-    }
-
-    input.setCustomValidity("");
+  if (dataFaz18 > dataAtual) {
+    input.setCustomValidity("A idade mínima é de 18 anos");
     return;
-}
+  }
+
+  input.setCustomValidity("");
+  return;
+};
